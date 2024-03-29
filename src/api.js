8f8699f31ef9276befdd821f7e88cad73b80293c
Willbemax <max22042007@yandex.ru>
2024-03-29 22:36:06 +0300

    reale fetch

--- src/api.js
+++ src/api.js
@@ -2,13 +2,27 @@ import { cryptoAssets, cryptoData } from './data';
 
 //Имуляция работы api
 
-export function fakeFetchCrypto() {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-            resolve(cryptoData)
-        }, 1)
-	});
-}
+export async function fakeFetchCrypto() {
+	try {
+	   const options = {
+		 method: 'GET',
+		 headers: {
+		   accept: 'application/json',
+		   'X-API-KEY': 'YHTMk24MyPu4g4bbTCcuTz4TFuQ4/iR8yT2JTs1rmZ0='
+		 }
+	   };
+   
+	   const response = await fetch('https://openapiv1.coinstats.app/coins', options);
+	   const result = await response.json();
+	   return result;
+	} catch (err) {
+	   console.error(err);
+	   return null;
+	}
+   }
+   
+
+
 export function fetchAssets() {
 	return new Promise((resolve) => {
 		setTimeout(() => {
